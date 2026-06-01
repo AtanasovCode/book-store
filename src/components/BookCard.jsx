36f527479300eaba7d61c6a3b1ce9cb093ebe72b
AtanasovCode <a_ljupce@yahoo.com>
2026-06-01 20:43:08 +0200

    add hover effects

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -20,6 +20,11 @@ const BookCard = ({ book }) => {
                 justifyContent: "center",
                 border: "1px solid #6f6c6c",
                 borderRadius: "16px",
+                transition: "all .3s ease-in-out",
+                cursor: "pointer",
+                "&:hover": {
+                    borderColor: "#fff"
+                }
             }}
         >
             <CardMedia
@@ -38,24 +43,25 @@ const BookCard = ({ book }) => {
             <Box
                 sx={{
                     width: "100%",
+                    p: 1,
                     display: "flex",
                     flexDirection: "column",
-                    alignItems: "flex-start",
+                    alignItems: "center",
                     justifyContent: "flex-start",
                     gap: 1,
                 }}
             >
-                <Typography variant="h5">
+                <Typography
+                    variant="h5"
+                    sx={{
+                        width: "100%",
+                        textAlign: "left"
+                    }}>
                     {book.name}
                 </Typography>
                 <Typography variant="body1">
                     ${book.price}
                 </Typography>
-                <Button
-                    variant="contained"
-                >
-                    See Details
-                </Button>
             </Box>
         </Card>
     );
